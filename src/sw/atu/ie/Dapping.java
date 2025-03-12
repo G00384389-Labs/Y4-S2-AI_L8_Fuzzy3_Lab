227c800e94b458dbf95283b51642b286c177d47d
MARK HUGHES - STUDENT <g00384389@atu.ie>
2025-03-12 15:42:29 +0000

    Lab Complete

---
+++ src/sw/atu/ie/Dapping.java
@@ -0,0 +1,27 @@
+package sw.atu.ie;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+
+public class Dapping {
+
+    public double getDappingLevel(int windBeaufort, int tempCelsius) { 
+        FIS fis = FIS.load("dappingSugeno.fcl", true); 
+        FunctionBlock fb = fis.getFunctionBlock("getDappingLevel"); 
+        fis.setVariable("wind", windBeaufort); 
+        fis.setVariable("temperature", tempCelsius); 
+        fis.evaluate(); 
+        Variable dapping = fb.getVariable("dapping"); 
+        JFuzzyChart.get().chart(fis); 
+        JFuzzyChart.get().chart(dapping.getDefuzzifier(), "Dapping Level", true); 
+        return dapping.getValue(); 
+    }
+    
+    public static void main(String[] args) {
+        Dapping dappingTest = new Dapping();
+        double result = dappingTest.getDappingLevel(8, 10); 
+        System.out.println("Dapping Level: " + result + "%");
+    }
+}
